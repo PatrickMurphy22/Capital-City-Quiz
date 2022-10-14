698890f828056b404b8b83048a184a527c43bf32
PatrickMurphy22 <Murphy866530@gmail.com>
2022-10-14 18:19:33 +0000

    added mediummode array with questions and answers

---
+++ assets/javascript/medium.js
@@ -0,0 +1,77 @@
+const mediumCountrys = [
+    {
+        country:("Afganistan"),
+        citys:["Kabul","","",""],
+        capital:("Kabul")
+    },
+    {
+        country:("Australia"),
+        citys:["Canberra","","",""],
+        capital:("Canberra")
+    },
+    {
+        country:("Cambodia"),
+        citys:["Phnom Penh","","",""],
+        capital:("Phnom Penh")
+    },
+    {
+        country:("Croatia"),
+        citys:["Zagreb","","",""],
+        capital:("Zagreb")
+    },
+    {
+        country:("Malaysia"),
+        citys:["Kuala Lumpur"," "," ",""],
+        capital:("Kuala Lumpur")
+    },
+    {
+        country:("Ghana"),
+        citys:["Accra","","",""],
+        capital:("Accra")
+    },
+    {
+        country:("Iraq"),
+        citys:["Baghdad","","",""],
+        capital:("Baghdad")
+    },
+    {
+        country:("Kenya"),
+        citys:["Nairobi","","",""],
+        capital:("Nairobi")
+    },
+    {
+        country:("Lebanon"),
+        citys:["Beirut","","",""],
+        capital:("Beirut")
+    },
+    {
+        country:("Morocco"),
+        citys:["Rabat"," ","",""],
+        capital:("Rabat")
+    },
+    {
+        country:("Nepal"),
+        citys:["Kathmandu","","",""],
+        capital:("Kathmandu")
+    },
+    {
+        country:("Peru"),
+        citys:["Lima","","",""],
+        capital:("Lima")
+    },
+    {
+        country:("New Zealand"),
+        citys:["Wellington","","",""],
+        capital:("Wellington")
+    },
+    {
+        country:("Brazil"),
+        citys:["Brasilia","","",""],
+        capital:("Brasilia")
+    },
+    {
+        country:("Vietnam"),
+        citys:["Hanoi","","",""],
+        capital:("Hanoi")
+    },
+]
