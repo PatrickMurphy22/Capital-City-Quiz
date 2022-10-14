c4a5b3277f1441c32359d5cba268fa9443393b34
PatrickMurphy22 <Murphy866530@gmail.com>
2022-10-14 18:19:58 +0000

    added Hardmode array with questions and answers

---
+++ assets/javascript/hard.js
@@ -0,0 +1,78 @@
+const hardCountrys = [
+    {
+        country:("Chad"),
+        citys:["N'Djamena","","",""],
+        capital:("N'Djamena")
+    },
+    {
+        country:("Colombia"),
+        citys:["Bogota","","",""],
+        capital:("Bogota")
+    },
+    {
+        country:("Ethiopia"),
+        citys:["Addis Ababa","","",""],
+        capital:("Addis Ababa")
+    },
+    {
+        country:("Indonesia"),
+        citys:["Jakarta","","",""],
+        capital:("Jakarta")
+    },
+    {
+        country:("Jordan"),
+        citys:["Amman","","",""],
+        capital:("Amman")
+    },
+    {
+        country:("Mozambique"),
+        citys:["Maputo","","",""],
+        capital:("Maputo")
+    },
+    {
+        country:("Trinidad and Tobago"),
+        citys:["Port of Spain","","",""],
+        capital:("Port of Spain")
+    },
+    {
+        country:("Libya"),
+        citys:["Tripoli","","",""],
+        capital:("Tripoli")
+    },
+    {
+        country:("Pakistan"),
+        citys:["Islamabad","","",""],
+        capital:("Islamabad")
+    },
+    {
+        country:("Albania"),
+        citys:["Tirana","","",""],
+        capital:("Tirana")
+    },
+    {
+        country:("Kosovo"),
+        citys:["Pristina","","",""],
+        capital:("Pristina")
+    },
+    {
+        country:("Cameroon"),
+        citys:["Yaounde","","",""],
+        capital:("Yaounde")
+    },
+    {
+        country:("Mongolia"),
+        citys:["Ulaanbaatar","","",""],
+        capital:("Ulaanbaatar")
+    },
+    {
+        country:("Uruguay"),
+        citys:["Montevideo","","",""],
+        capital:("Montevideo")
+    },
+    {
+        country:("Saudi Arabia	"),
+        citys:["Riyadh","","",""],
+        capital:("Riyadh")
+    },
+
+]
