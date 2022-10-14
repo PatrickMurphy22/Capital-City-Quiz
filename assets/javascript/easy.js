15a3cb6a0769fb3193253c8570e5fb08df30dded
PatrickMurphy22 <Murphy866530@gmail.com>
2022-10-14 18:19:06 +0000

    added easymode array with questions and answers

---
+++ assets/javascript/easy.js
@@ -0,0 +1,78 @@
+// List of EASY countrys in array format
+const easyCountrys= [
+    {
+        country:("Ireland"),
+        citys:["Dublin","Limerick","Cork","Galway"],
+        capital:["Dublin"]
+    },
+    {
+        country:("England"),
+        citys:["London","Liverpool","Manchester","Newcastle"],
+        capital:("London")
+    },
+    {
+        country:("Scotland"),
+        citys:["Edinburgh","Glasgow","Aberdeen","Dundee"],
+        capital:("Edinburgh")
+    },
+    {
+        country:("Sweden"),
+        citys:["Stockholm","Malmo","Gothenburg","Helsinburg"],
+        capital:("Stockholm")
+    },
+    {
+        country:("USA"),
+        citys:["Washington DC","New York","San Francisco","Washington"],
+        capital:("Washington DC")
+    },
+    {
+        country:("France"),
+        citys:["Paris","Marseille","Nice","Lyon"],
+        capital:("Paris")
+    },
+    {
+        country:("Mexico"),
+        citys:["Mexico City","Cancun","Guadalajara","Merida"],
+        capital:("Mexico City")
+    },
+    {
+        country:("China"),
+        citys:["Beijing","Shanghi","Wuhan","Chongqing"],
+        capital:("Beijing")
+    },
+    {
+        country:("Japan"),
+        citys:["Tokyo","Kyoto","Osaka","Hiroshima"],
+        capital:("Tokyo")
+    },
+    {
+        country:("Thailand"),
+        citys:["Bangkok","Chaing Mai","Phuket","Pattaya"],
+        capital:("Bangkok")
+    },
+    {
+        country:("South Korea"),
+        citys:["Seoul","Busan","Pyongyang","incheon"],
+        capital:("Seoul")
+    },
+    {
+        country:("Spain"),
+        citys:["Madrid","Barcelona","Malaga","Sevilla"],
+        capital:("Madrid")
+    },
+    {
+        country:("Poland"),
+        citys:["Warsaw","Krakow","Wroclaw","Gdansk"],
+        capital:("Poland")
+    },
+    {
+        country:("Germany"),
+        citys:["Berlin","Stutgart","Hamburg","Frankfurt"],
+        capital:("Berlin")
+    },
+    {
+        country:("Italy"),
+        citys:["Rome","Venice","Milan","Naples"],
+        capital:("Rome")
+    },
+]
